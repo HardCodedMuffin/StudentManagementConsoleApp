62f34959d2c5222e794cd3000e445fd82ae6a101
Ömer Çelik <0.omarclk@gmail.com>
2019-11-18 12:16:41 +0300

    Update StudentManagementCnslApp.cpp

--- StudentManagementCnslApp.cpp
+++ StudentManagementCnslApp.cpp
@@ -1,58 +1,74 @@
 #include <iostream>
 #include <stdio.h>
-#include "Student.h"								//Including student structure and function prototypes
+#include "Student.h"
+#include "Student.cpp"     //Including student structure and function prototypes
+#include <string>
+using namespace std;
 
-int main()
-{
-	Student stu;									//Creating student object to access non static functions
-	char choice;
+int main() {
+	Student stu;     //Creating student object to access non static functions
+	string ChosenNumber;
 
-	stu.start_stream();								//A function that creates a file and establishing the connection 
 
-	while (1)
-	{
-		system("cls");								//Clearing the console display of all text and positioning the text cursor in the home location
+	stu.start_stream();   //A function that creates a file and establishing the connection
 
-		cout << "\t\t====== STUDENT DATABASE MANAGEMENT SYSTEM ======";
-		cout << "\n\n                                          ";
-		cout << "\n\n";
-		cout << "\n \t\t\t 1. Add    Records";
-		cout << "\n \t\t\t 2. List   Records";
-		cout << "\n \t\t\t 3. Search Records";
-		cout << "\n \t\t\t 4. Modify Records";
-		cout << "\n \t\t\t 5. Delete Records";
-		cout << "\n \t\t\t 6. Exit   Program";
-		cout << "\n\n";
-		cout << "\t\t\t Select Your Choice :=> ";
-		fflush(stdin);								//Flushes the output buffer and move the buffered data to console (in case of stdout) or disk (in case of file output stream)
-		cin >> choice;
 
-		switch (choice)								//A switch statement for option handling
-		{
-		case '1':
-			stu.add_record();						
-			break;
-		case '2':
-			stu.list_records();			
-			break;
-		case '3':
-			system("cls");
-			stu.search_record();
-			break;
-		case '4':
-			system("cls");
-			stu.modify_record();
-			break;
-		case '5':
-			system("cls");
-			stu.delete_record();
-			break;
+	cout << "                      .   *        .       .\n"
+		"          .                .  *       - )-\n"
+		"       .      *       o       .       *\n"
+		" o                |\n"
+		"           .     -O-\n"
+		".                 |        *      .     -0-\n"
+		"                .           o\n"
+		"        .---.      Welcome to Student Database Program\n"
+		"  =   _/__~0_\\_     .  *     Hold on to your spaceship!\n"
+		" = = (_________)             .\n"
+		"                 .                        *\n"
+		"       *               - ) -       *"
+		"\n1. Enter a new student record."
+		"\n2. List all records."
+		"\n3. Modify a student."
+		"\n4. Delete a record."
+		"\n5. Search by student ID, surname or taken courses."
+		"\nTo enter an operation, type the ID of it."
+		"\nMy number is: ";
+	fflush(stdin);    //Flushes the output buffer and move the buffered data to console (in case of stdout) or disk (in case of file output stream)
 
-		case '6':
-			stu.close_stream();						//A function that ends up the file stream and makes resources available again
-			break;
+	while (true) {
+		getline(cin, ChosenNumber);
+		try {
+			int parsed = stoi(ChosenNumber);
+			if (parsed < 5) {
+				break;
+			}
 		}
+		catch (std::invalid_argument) {
+			cout << "Wrong type of input has entered." << endl;
+		}
+		cout << "\nCan you just try again? But carefully?" << endl << "Enter the number again: ";
+	}
+	int choice = stoi(ChosenNumber);
+	switch (choice) {
+	case 1:
+		Student::add_record();
+		break;
+
+
+	case 2:
+		Student::list_records();
+		break;
+
+	case 3:
+		Student::modify_record();
+		break;
+
+	case 4:
+		Student::delete_record();
+		break;
+
+	case 5:
+		Student::search_record();
+
 	}
 
-	return 0;
 }
