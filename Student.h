10336d62561a2af8c0b17db827cb1cfd11ea29d4
Ömer Çelik <0.omarclk@gmail.com>
2019-11-18 12:15:11 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -6,16 +6,16 @@ using namespace std;
 
 struct Student
 {
-	char first_name[50], last_name[50], course[100], id[20];
-	int section;
+    char first_name[50], last_name[50], course[100], id[20];
+    int section;
 
-	int start_stream();
-	void add_record();
-	void list_records();
-	void search_record();
-	void modify_record();
-	void delete_record();
-	void close_stream();
+    static int start_stream();
+    static void add_record();
+    static void list_records();
+    static void search_record();
+    static void modify_record();
+    static void delete_record();
+    void close_stream();
 };
 #endif // !Student_H
 
