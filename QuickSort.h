9156f439144d013473085bdd16151d54dd506493
Ömer Çelik <s170444022@stu.thk.edu.tr>
2019-12-08 15:05:17 +0300

    Delete QuickSort.h

--- QuickSort.h
+++
@@ -1,8 +0,0 @@
-#ifndef QuickSort_H
-#define QuickSort_H
-
-void swap(int a, int b);
-int partition(char items[], int low, int high);
-void quickSort(char items[], int low, int high);
-
-#endif // !QuickSort
